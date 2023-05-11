a03bcd78ae288c83c1b2f6fdf9703a0a287b9cd0
rodri <rodrigomatamorosbecerra@gmail.com>
2023-05-11 19:06:59 +0200

    Commit Directorio

---
+++ src/main/java/org/example/Directorio.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+public class Directorio implements Componente {
+    private String nombre;
+    private List<Componente> componentes;
+
+    public Directorio(String nombre) {
+        this.nombre = nombre;
+        this.componentes = new ArrayList<>();
+    }
+
+    @Override
+    public String getNombre() {
+        return nombre;
+    }
+
+    @Override
+    public int getTamano() {
+        int total = 0;
+        for (Componente componente : componentes) {
+            total += componente.getTamano();
+        }
+        return total;
+    }
+
+    public void agregarComponente(Componente componente) {
+        componentes.add(componente);
+    }
+}
